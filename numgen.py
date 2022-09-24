03cc69831169358c54c2a20d47e092ec1a536027
neptunnx <113943092+neptunnx@users.noreply.github.com>
2022-09-24 10:08:28 -0400

    Create numgen.py
    Signed-off-by: neptunnx <113943092+neptunnx@users.noreply.github.com>

---
+++ numgen.py
@@ -0,0 +1,11 @@
+import random
+def random_phone_num_generator():
+    first = str(random.randint(100, 999))
+    second = str(random.randint(1, 888)).zfill(3)
+    last = (str(random.randint(1, 9998)).zfill(4))
+    while last in ['1111', '2222', '3333', '4444', '5555', '6666', '7777', '8888']:
+        last = (str(random.randint(1, 9998)).zfill(4))
+    return '{}-{}-{}'.format(first, second, last)
+n = int(input("Enter The amount of numbers you want to gen: "))
+for i in range(0, n):
+    print(random_phone_num_generator())
