14b164de28a68a58bf4282080f700a877c9c81cc
neptunnx <113943092+neptunnx@users.noreply.github.com>
2022-09-24 10:47:05 -0400

    Create nitrocheck.py
    Signed-off-by: neptunnx <113943092+neptunnx@users.noreply.github.com>

---
+++ nitrocheck.py
@@ -0,0 +1,16 @@
+import requests
+import random
+import string
+
+with open("Nitro Codes.txt") as file:
+    for line in file.readlines():
+        nitro = line.strip("\n")
+
+        url = f"https://discordapp.com/api/v9/entitlements/gift-codes/{nitro}?with_application=false&with_subscription_plan=true"
+
+        r = requests.get(url)
+
+        if r.status_code == 200:
+            print(f"\n\n Valid | {nitro}\n\n") 
+        else:
+            print(f"*", end = "")
